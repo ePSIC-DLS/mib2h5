e48498ec0252c845f2b085b3cdc86d8f9ec6ccb8
Sze Ching <sze.ching@diamond.ac.uk>
2025-04-08 09:06:31 +0100

    Changes in config.mk to add different build targets, emit error message when no HDF5_ROOT path

--- config.mk
+++ config.mk
@@ -1,20 +1,37 @@
 # === Directories ===
 SRCDIR := src
 OBJDIR := obj
-BINDIR := bin
+BINDIR ?= bin
 INCDIR := include
-HDFDIR := $(HDF5_ROOT)
+HDFDIR ?= $(HDF5_ROOT)
+
+BUILD ?= debug
+
+ifndef HDFDIR
+$(error HDFDIR is not defined. Please set HDF5_ROOT or pass HDFDIR=...)
+endif
 
 # === Compiler and flags ===
 CC := gcc
-CFLAGS := -Wall -Wextra -std=c11 -g -O0 -fanalyzer -I$(SRCDIR) -I$(INCDIR) -I$(HDFDIR)/include
-LDFLAGS := -L$(HDFDIR)/lib -lhdf5
+
+ifeq ($(BUILD),debug)
+	CFLAGS := -Wall -Wextra -std=c11 -g -O0 -fanalyzer
+else ifeq ($(BUILD),release)
+	CFLAGS := -Wall -Wextra -std=c11 -O2
+else ifeq ($(BUILD),asan)
+	CFLAGS := -Wall -Wextra -std=c11 -g -O0 -fsanitize=address
+	LDFLAGS += -fsanitize=address
+else
+	$(error Unknown BUILD type: $(BUILD))
+endif
+
+CFLAGS += -I$(SRCDIR) -I$(INCDIR) -I$(HDFDIR)/include
+LDFLAGS += -L$(HDFDIR)/lib -lhdf5
 
 # === Sources ===
 
 SOURCES := $(wildcard $(SRCDIR)/*.c)
 
-OBJECTS := \
-    $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES)) \
+OBJECTS := $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES))
 
-TARGET := $(BINDIR)/main.out
+TARGET := $(BINDIR)/mib2h5
