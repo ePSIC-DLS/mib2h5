8f587efc9473abece68509967bf3451181c3b4be
Sze Ching <sze.ching@diamond.ac.uk>
2025-04-28 11:03:28 +0100

    Modifying references in header file for create_merlin_dataset

--- src/hdf5_init.h
+++ src/hdf5_init.h
@@ -16,15 +16,12 @@ void initialize_plist(hid_t *file_id,
 void initialize_file(char *filename, hid_t *file_id, hid_t fapl, hid_t fcpl);
 
 void create_merlin_dataset(hid_t *merlin_dataset_id,
-                           hid_t *file_id,
+                           hid_t file,
                            char *merlin_dataset_name,
                            int dtype,
                            hid_t memspace,
-                           hid_t *lcpl_id,
+                           hid_t lcpl,
                            size_t dim,
-                           hsize_t *frame_dim,
-                           unsigned int compression_level,
-                           unsigned int shuffle,
-                           unsigned int compressor);
+                           hsize_t *frame_dim);
 
 #endif
