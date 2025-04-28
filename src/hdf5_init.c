bae4d3b00196d0286dbf7d39acbf8c7d6d7cafc2
Sze Ching <sze.ching@diamond.ac.uk>
2025-04-28 11:11:27 +0100

    Create function bufsize_to_datatype to handle logic related to different data size

--- src/hdf5_init.c
+++ src/hdf5_init.c
@@ -42,6 +42,34 @@ void initialize_file(char *filename, hid_t *file_id, hid_t fapl, hid_t fcpl)
   }
 }
 
+hid_t bufsize_to_datatype(int dtype)
+{
+  hid_t datatype;
+  switch (dtype) {
+    case 1: {
+      datatype = H5T_STD_U8LE;
+      break;
+    }
+    case 2: {
+      datatype = H5T_STD_U16LE;
+      break;
+    }
+    case 4: {
+      datatype = H5T_STD_U32LE;
+      break;
+    }
+    case 8: {
+      datatype = H5T_STD_U64LE;
+      break;
+    }
+    default: {
+      fprintf(stderr, "Error in datatype, please check input dtype\n");
+      return;
+    }
+  }
+  return datatype;
+}
+
 void create_merlin_dataset(hid_t *merlin_dataset_id,
                            hid_t file,
                            char *merlin_dataset_name,
@@ -81,29 +109,7 @@ void create_merlin_dataset(hid_t *merlin_dataset_id,
     }
   }
 
-  hid_t datatype;
-  switch (dtype) {
-    case 1: {
-      datatype = H5T_STD_U8LE;
-      break;
-    }
-    case 2: {
-      datatype = H5T_STD_U16LE;
-      break;
-    }
-    case 4: {
-      datatype = H5T_STD_U32LE;
-      break;
-    }
-    case 8: {
-      datatype = H5T_STD_U64LE;
-      break;
-    }
-    default: {
-      fprintf(stderr, "Error in datatype, please check input dtype\n");
-      return;
-    }
-  }
+  hid_t datatype = bufsize_to_datatype(dtype);
 
   if ((*merlin_dataset_id = H5Dcreate2(file, merlin_dataset_name, datatype,
                                        memspace, lcpl, dcpl, dapl)) ==
--- src/hdf5_init.h
+++ src/hdf5_init.h
@@ -3,10 +3,8 @@
 #define HDF5_INIT_H
 
 #include <hdf5.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void initialize_plist(hid_t *file_id,
                       hid_t *fapl_id,
@@ -14,6 +12,8 @@ void initialize_plist(hid_t *file_id,
                       hid_t *lcpl_id);
 
 void initialize_file(char *filename, hid_t *file_id, hid_t fapl, hid_t fcpl);
+
+hid_t bufsize_to_datatype(int dtype);
 
 void create_merlin_dataset(hid_t *merlin_dataset_id,
                            hid_t file,
