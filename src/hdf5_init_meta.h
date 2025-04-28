db4d926632f1e26550c5110ea06ef62e89f90773
Sze Ching <sze.ching@diamond.ac.uk>
2025-04-28 13:56:53 +0100

    Modify references in create_meta_dataset and create_dac_dataset

--- src/hdf5_init_meta.h
+++ src/hdf5_init_meta.h
@@ -9,13 +9,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-void create_meta_mq1_fields_dataset(hid_t *file_id,
-                                    hid_t *lcpl_id,
-                                    hid_t *meta_handle);
+void create_meta_mq1_fields_dataset(hid_t file, hid_t lcpl, hid_t *meta_handle);
 
 void create_dac_dataset(unsigned int num_chips,
-                        hid_t *file_id,
-                        hid_t *lcpl_id,
+                        hid_t file,
+                        hid_t lcpl,
                         hid_t *dac_handle);
 
 void close_dataset_handle(hid_t *handle, size_t count);
