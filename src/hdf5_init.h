8735420e75ab22255e7c45198d69fbd14d5184d6
Sze Ching <sze.ching@diamond.ac.uk>
2025-04-28 10:35:29 +0100

    Changes to references in initialize_plist and initialize_file

--- src/hdf5_init.h
+++ src/hdf5_init.h
@@ -8,12 +8,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-void initialize_file_and_plist(char *filename,
-                               hid_t *file_id,
-                               hid_t *fapl_id,
-                               hid_t *fcpl_id);
+void initialize_plist(hid_t *file_id,
+                      hid_t *fapl_id,
+                      hid_t *fcpl_id,
+                      hid_t *lcpl_id);
 
-void initialize_lcpl(hid_t *lcpl_id);
+void initialize_file(char *filename, hid_t *file_id, hid_t fapl, hid_t fcpl);
 
 void create_merlin_dataset(hid_t *merlin_dataset_id,
                            hid_t *file_id,
