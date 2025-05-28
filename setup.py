7a78ea44fc29a88de7f8eebb8c3a9bacbb74b279
Sze Ching <teo.ching@diamond.ac.uk>
2025-05-28 16:24:23 +0100

    Add checks for header file

--- setup.py
+++ setup.py
@@ -3,21 +3,42 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
+import sys
+
+def assert_file_exists(path, filename):
+  full_path = os.path.join(path, filename)
+  if not os.path.exists(full_path):
+    print(f"{filename} not found in {path}")
+    return False
+  else:
+    print(f"{filename} found in {path}")
+    return True
+
+
 
 HDF5_ROOT = os.environ.get("HDF5_ROOT", "/usr")
 
 hdf5_include = os.path.join(HDF5_ROOT, "include")
 hdf5_lib = os.path.join(HDF5_ROOT, "lib")
 
+if not assert_file_exists(hdf5_include, "hdf5.h"):
+  sys.exit(1)
+
 BLOSC_ROOT = os.environ.get("BLOSC_ROOT", "/usr")
 
 blosc_include = os.path.join(BLOSC_ROOT, "include")
 blosc_lib = os.path.join(BLOSC_ROOT, "lib64")
 
+if not assert_file_exists(blosc_include, "blosc.h"):
+  sys.exit(1)
+
 HDF5_BLOSC_ROOT = os.environ.get("HDF5_BLOSC_ROOT", "/usr")
 
 hdf5_blosc_include = os.path.join(HDF5_BLOSC_ROOT, "src")
 hdf5_blosc_lib = os.path.join(HDF5_BLOSC_ROOT, "build")
+
+if not assert_file_exists(hdf5_blosc_include, "blosc_filter.h"):
+  sys.exit(1)
 
 ext = Extension(
   name="mib2h5_wrapper",
