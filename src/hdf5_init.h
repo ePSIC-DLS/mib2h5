f790d2ac7485aa710869ac1cbb2542cdc2474aeb
Timothy Poon <timothy0626@gmail.com>
2025-07-07 22:53:00 +0100

    Create expandable initial dataset for Merlin frames

--- src/hdf5_init.h
+++ src/hdf5_init.h
@@ -15,11 +15,9 @@ void initialize_file(char *filename, hid_t *file_id, hid_t fapl, hid_t fcpl);
 void create_merlin_dataset(hid_t *merlin_dataset_id,
                            hid_t file,
                            char *merlin_dataset_name,
-                           int dtype,
-                           hid_t memspace,
+                           hid_t dtype,
                            hid_t dcpl,
                            hid_t lcpl,
-                           size_t dim,
                            hsize_t *frame_dim);
 
 #endif
