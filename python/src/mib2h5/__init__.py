fb27e8f3039d669e14653d51b241882b6221065f
Timothy Poon <timothy0626@gmail.com>
2025-08-28 09:41:10 +0100

    Provide __verision__ attribute for backwards compatibility

--- python/src/mib2h5/__init__.py
+++ python/src/mib2h5/__init__.py
@@ -4,6 +4,13 @@ This package provides Python binding for converting MerlinEM MIB files
 to HDF5 files.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from ._wrapper import convert
 
-__all__ = ["convert"]
+try:
+    __version__ = version("mib2h5")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["convert", "__version__"]
